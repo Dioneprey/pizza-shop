08f041855a72cfb9660fe2aa430ba65b15362d02
Dionatan <dionatan.1751@gmail.com>
2024-01-21 23:10:34 -0300

    setup, shadcn, react routes etc

---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
+import { Dashboard } from './pages/app/dashboard'
+import { SignIn } from './pages/auth/sign-in'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+  {
+    path: '/login',
+    element: <AuthLayout />,
+    children: [{ path: '/login', element: <SignIn /> }],
+  },
+])
